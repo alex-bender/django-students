f089c86a663702e027b7f25e87f4d1da24f17d3d
Alex_bender <axbndr@gmail.com>
2013-03-02 16:04:26 +0200

    added registration

--- students/main/views.py
+++ students/main/views.py
@@ -1,96 +1,155 @@
 from django.http import HttpResponse
-from django.shortcuts import render_to_response, redirect, get_object_or_404
+from django.shortcuts import render_to_response, redirect, get_object_or_404, render
 from django.views.generic.simple import direct_to_template
 from django.template import RequestContext
 
-from models import Student, Group, StudentForm, GroupForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+from forms import Student, Group, StudentForm, GroupForm, UserCreateForm#, AddUserForm
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+def index(request):
+    return HttpResponse('something')
+#------------------------------------------------------------------------------
+#            logout, login, and add to ech other function (if user.auth) ....
+#------------------------------------------------------------------------------
+def auth_user(request, username, password):
+#    user = authenticate(username=request.POST['username'],
+#                        password=request.POST['password'])
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return HttpResponse('user is logined')
+        else:
+            return HttpResponse('disabled acc')
+    else:
+        return HttpResponse('bad login')
 
 
+def CreateUserAndLogin(request, *args, **kwargs):
+    user_form = UserCreateForm(request.POST)
+    if user_form.is_valid():
+        username = user_form.clean_username()
+        password = user_form.clean_password2()
+        user_form.save()
+#        user = authenticate(username=username,
+#                            password=password)
+#        redirect to lodin function
+#        login(request, user)
+        return redirect(index)
+    return render(request,
+                  'atemlete.html',
+                  { 'user_form' : user_form })
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        # Правильный пароль и пользователь "активен"
+        auth.login(request, user)
+        # Перенаправление на "правильную" страницу
+        return HttpResponseRedirect("/account/loggedin/")
+    else:
+        # Отображение страницы с ошибкой
+        return HttpResponseRedirect("/account/invalid/")
+
+
+#def new_user(request):
+#    form = AddUserForm(request.POST or None) 
+#    if form.is_valid():
+#        cmodel = form.save()
+#        cmodel.save()
+#        return redirect(students)
+#    return render_to_response('registration.html',
+#                              {'form': form,},
+#                               context_instance=RequestContext(request))
+
+#
+#def render_user_form(request):
+#    if request.method == 'POST': # If the form has been submitted...
+#        form = ContactForm(request.POST) # A form bound to the POST data
+#        if form.is_valid(): # All validation rules pass
+#            # Process the data in form.cleaned_data
+#            # ...
+#            return HttpResponseRedirect('/thanks/') # Redirect after POST
+#    else:
+#        form = ContactForm() # An unbound form
+#
+#    return render(request, 'contact.html', {
+#        'form': form,
+#    })
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 def students(request):
     students_list = Student.objects.all().order_by('name')
     return render_to_response('students.html',
                               {'students_list': students_list,})
-
+#------------------------------------------------------------------------------
 def students_add(request):
-    # sticks in a POST or renders empty form
     form = StudentForm(request.POST or None)
     if form.is_valid():
         cmodel = form.save()
-        #This is where you might chooose to do stuff.
-        #cmodel.name = 'test1'
         cmodel.save()
         return redirect(students)
 
     return render_to_response('students_add.html',
                               {'student_form': form},
                               context_instance=RequestContext(request))    
-
-#------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 def students_edit(request, student_id):
     student = get_object_or_404(Student, pk=student_id)
-    
-    return redirect(students)
+    form = StudentForm(request.POST or None, instance=student)
+    if form.is_valid():
+        concat = form.save()
+        concat.save()
+        return redirect(students)
+    return render_to_response('students_edit.html',
+                              {'student_form': form,
+                               'student_id': student_id},
+                              context_instance=RequestContext(request))
 #------------------------------------------------------------------------------
-#------------------------------------------------------------------------------
-
 def students_delete(request, student_id):
     stud = Student.objects.get(pk=student_id).delete()
 
     return redirect(students)
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
-
-
-
-
-
-def contact_edit(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id)
-    form = ContactForm(request.POST or None, instance=contact)
+def groups(request):
+    groups_list = Group.objects.all().order_by('name')
+    return render_to_response('groups.html',
+                              {'groups_list': groups_list,})
+#------------------------------------------------------------------------------
+def groups_add(request):
+    form = GroupForm(request.POST or None)
     if form.is_valid():
-        contact = form.save()
-        #this is where you might choose to do stuff.
-        #contact.name = 'test'
-        contact.save()
-        return redirect(contacts)
-
-    return render_to_response('contact_edit.html',
-                              {'contact_form': form,
-                               'contact_id': contact_id},
+        cmodel = form.save()
+        cmodel.save()
+        return redirect(groups)
+    
+    return render_to_response('groups_add.html',
+                              {'group_form': form},
                               context_instance=RequestContext(request))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #------------------------------------------------------------------------------
+def groups_edit(request, group_id):
+    group = get_object_or_404(Group, pk=group_id)
+    form = GroupForm(request.POST or None, instance=group)
+    if form.is_valid():
+        concat = form.save()
+        concat.save()
+        return redirect(groups)
+    return render_to_response('groups_edit.html',
+                              {'group_form': form,
+                               'group_id': group_id},
+                              context_instance=RequestContext(request))    
 #------------------------------------------------------------------------------
-
+def groups_delete(request, group_id):
+    group = Group.objects.get(pk=group_id).delete()
+    
+    return redirect(groups)
 #------------------------------------------------------------------------------
-#------------------------------------------------------------------------------ 
-def groups_list(request):
-    pass
-def groups_add(request, group):
-    pass
-def groups_edit(request, group):
-    pass
-def groups_delete(request, group):
-    pass
--- students/students/urls.py
+++ students/students/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from main.views import *
+from main.views import students, students_add,  students_edit,students_delete
+from main.views import groups, groups_add, groups_edit, groups_delete
+from main.views import  auth_user, post#, new_user,
 
 admin.autodiscover()
 
@@ -15,15 +17,21 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    
-#-------------------------------------------------------------------------------    
+
+    url(r'^auth/(?P<username>\w+)/(?P<password>\w+)/', 'main.views.auth_user'),
+    url(r'^new_user/$', 'main.views.CreateUserAndLogin'),
+
     url(r'^students/$', 'main.views.students'),
     url(r'^students_add/$', 'main.views.students_add'),
     url(r'^students_edit/(?P<student_id>\d+)/$','main.views.students_edit'),
     url(r'^students_delete/(?P<student_id>\d+)/$','main.views.students_delete'),
+#------------------------------------------------------------------------------
+    url(r'^groups/$', 'main.views.groups'),
+    url(r'^groups_add/$', 'main.views.groups_add'),
+    url(r'^groups_edit/(?P<group_id>\d+)/$','main.views.groups_edit'),
+    url(r'^groups_delete/(?P<group_id>\d+)/$','main.views.groups_delete'),
+#------------------------------------------------------------------------------
+    url(r'^registration/$', 'main.views.new_user'),
     
-#-------------------------------------------------------------------------------
-    url(r'^contact_add/$', 'main.views.contact_add'),
-    url(r'^contact_edit/(?P<contact_id>\d+)/$','main.views.contact_edit'),
-    url(r'^contact_delete/(?P<contact_id>\d+)/$','main.views.contact_delete'),
+
 )
